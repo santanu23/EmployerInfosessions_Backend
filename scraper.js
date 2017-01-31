b6dfd7f2a67486290830ceaa6ee1db4ec6fbe21c
Santanu Sarker <sarkersantanu@gmail.com>
2017-01-30 23:54:44 -0500

    added async calls

--- scraper.js
+++ scraper.js
@@ -1,32 +1,40 @@
 var express = require('express');
+var async = require('async');
 var Scraper = require ('images-scraper');
 
 var app = express();
-//var google = new Scraper.Google(); 
-var bing = new Scraper.Bing()
-
+let bing = new Scraper.Bing();
 app.get('/getImage', function (req, res) {
-	console.log("Request:" + req.query.employer);
-  var searchString = req.query.employer + " logo";
-  bing.list({
-    keyword: searchString,
-    num: 1,
-    detail: false
-  })
-  .then(function (result) {
-    imageUrl = result[0].url;
-    console.log(result);
-    res.send(imageUrl);
-  }).catch(function(err) {
-    console.log('err',err);
+  employerList =  req.query.employerList;
+  let calls = [];
+  Object.keys(employerList).forEach(function(key) {
+      calls.push(function(callback){
+        bing.list({
+        keyword: employerList[key] + " logo",
+        num: 1,
+        detail: false
+      })
+      .then(function (result) {
+        console.log("Request:" + req.query.employerList); 
+        imageUrl = result[0].url;
+        console.log(result);
+        callback(null, imageUrl);
+      }).catch(function(err) {
+        console.log('err',err);
+        callback(err);
+      });
+      });
+  });
+  async.parallel(calls, function(err, result) {     
+    res.send(result);
+    if (err)
+        return console.log(err);
   });
 });
 
 var server = app.listen(8081, function () {
-
   var host = server.address().address
   var port = server.address().port
-
   console.log("Example app listening at http://%s:%s", host, port)
 
 })
