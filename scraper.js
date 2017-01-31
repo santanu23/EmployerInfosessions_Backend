44d12022f3a29168403d7bd4b5787dc3fa6e0817
Santanu Sarker <sarkersantanu@gmail.com>
2017-01-30 22:35:41 -0500

    use bing for faster result

--- scraper.js
+++ scraper.js
@@ -2,24 +2,24 @@ var express = require('express');
 var Scraper = require ('images-scraper');
 
 var app = express();
-var google = new Scraper.Google(); 
+//var google = new Scraper.Google(); 
+var bing = new Scraper.Bing()
 
 app.get('/getImage', function (req, res) {
 	console.log("Request:" + req.query.employer);
-    google.list({
-    	keyword: 'google logo',
-   		num: 1,
-   		detail: false,
-   		nightmare: {
-        	show: false
-    	}
-	})
-	.then(function (result) {
-    	console.log('first result from google', result);
-    	res.send(result);
-	}).catch(function(err) {
-    	console.log('err', err);
-	});
+  var searchString = req.query.employer + " logo";
+  bing.list({
+    keyword: searchString,
+    num: 1,
+    detail: false
+  })
+  .then(function (result) {
+    imageUrl = result[0].url;
+    console.log(result);
+    res.send(imageUrl);
+  }).catch(function(err) {
+    console.log('err',err);
+  });
 });
 
 var server = app.listen(8081, function () {
